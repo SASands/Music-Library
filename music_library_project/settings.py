0470484a7f200e257b5f59d258d7c70380b5add9
SASands <snads@live.com>
2022-04-25 13:53:03 -0400

    Changed database settings and created music app

--- music_library_project/settings.py
+++ music_library_project/settings.py
@@ -119,3 +119,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from music_library_project.local_settings import *
+except ImportError:
+    pass
