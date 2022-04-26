e9117fe0d193e28670b2d80e1349cace43e8e837
SASands <snads@live.com>
2022-04-26 12:31:17 -0400

    finished my crud function in views file + updated the urls + tested all in Postman, need to figure out how to return the deleted song in the terminal

--- music/models.py
+++ music/models.py
@@ -1,5 +1,4 @@
 from datetime import date
-from platform import release
 from django.db import models
 
 # Create your models here.
--- music/views.py
+++ music/views.py
@@ -1,7 +1,10 @@
 from distutils import errors
 from distutils.log import error
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+
+import music
 from .serializers import SongSerializer
 from .models import Song
 from music import serializers
@@ -20,8 +23,28 @@ def songs_list(request):
         serializer = SongSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
       
+@api_view(["GET", "PUT", "DELETE"])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(song.delete, status=status.HTTP_200_OK)
+
+
+
+
+
+
 
 
     
